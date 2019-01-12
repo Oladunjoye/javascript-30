b147e217eb89d6dcb6231a30223d613435b5c554
joye <shonubij@gmail.com>
2019-01-12 11:58:25 +0100

    a clock running on javascript

---
+++ clock.js/app.js
@@ -0,0 +1,29 @@
+
+const secondHand = document.querySelector('.second-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const hourHand = document.querySelector('.hour-hand')
+
+function setDate() {
+    const now = new Date()
+
+    // seconds 
+    const seconds = now.getSeconds()
+    const secondsDegrees = ((seconds/60) * 360) + 90
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+
+    // minutes
+    const minutes = now.getMinutes()
+    const minutesDegrees = ((minutes/60) * 360) + 90
+   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+
+    // hours
+    const hours = now.getHours()
+    const hoursDegrees = ((hours/12) * 360) + ((mins/60) * 30) + 90
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+
+}
+
+setInterval(setDate,1000);
+setDate()
